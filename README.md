e8a9a3d34df39d8cbc3dba7814412f498c1ed1c0
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-08-30 15:54:36 +0900

    feat: REST API

---
+++ README.md
@@ -0,0 +1,36 @@
+# Node.js + MySQL REST API
+Node.jsとMySQLを使ったREST APIのサンプルです。
+
+## 使い方
+Macの人は、homebrewでMySQLをインストールしてください。
+```bash
+brew install mysql
+```
+
+shopテーブルという自動連番のIDとタイムスタンプがあるテーブルを作成します。
+
+shopテーブルを作成します。
+```sql
+--テーブル作成。Timestampは入力されなかったら自動で挿入される--
+CREATE TABLE shop (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  address VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+);
+
+--テーブルの構造を確認--
+DESC shop;
+
+---データ挿入---
+INSERT INTO shop (name, address) VALUES ('test1', 'address1');
+INSERT INTO shop (name, address) VALUES ('test2', 'address2');
+INSERT INTO shop (name, address) VALUES ('test3', 'address3');
+
+--データを取得--
+SELECT * FROM shop LIMIT 3;
+--id1を取得--
+SELECT * FROM shop WHERE id = 1;
+```
---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql2");
+const cors = require("cors");
+// corsを使うと、クロスオリジンのエラーが出なくなる
+app.use(cors());
+// express.json()を使うと、req.bodyを使えるようになる
+app.use(express.json());
+
+// MySQLの接続情報
+const db = mysql.createConnection({
+  user: "root",// 作成したユーザー名
+  host: "localhost",// host名
+  password: "1234",// 作成したユーザーのパスワード
+  database: "MyData",// 作成したデータベース名
+  port: 3306, // MySQLのデフォルトポート
+});
+
+// データの追加
+app.post("/shop", (req, res) => {
+  const name = req.body.name;
+  const address = req.body.address;
+
+  const query = "INSERT INTO shop (name, address) VALUES (?, ?)";
+  db.query(query, [name, address], (err, result) => {  // addressを追加
+    if (err) {
+      console.log(err);
+      res.status(500).send({ error: "Error inserting data into database" });
+    } else {
+      res.status(200).json({ message: "Value Inserted" });
+    }
+  });
+});
+
+
+
+// データの取得
+app.get("/shop", (req, res) => {
+  const query = "SELECT * FROM shop";
+  db.query(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Error retrieving data from database");
+    } else {
+      res.status(200).json(result);
+    }
+  });
+});
+
+// データの更新
+app.put("/shop/:id", (req, res) => {
+  const id = req.params.id;
+  const name = req.body.name;
+  const address = req.body.address;  // これを追加
+
+  const query = "UPDATE shop SET name = ?, address = ? WHERE id = ?";
+  db.query(query, [name, address, id], (err, result) => {  // address と id の順番に注意
+    if (err) {
+      console.log(err);
+      res.status(500).send("Error updating data in database");
+    } else {
+      res.status(200).send("Value Updated");
+    }
+  });
+});
+
+// データの削除
+app.delete("/shop/:id", (req, res) => {
+  const id = req.params.id;
+
+  const query = "DELETE FROM shop WHERE id = ?";
+  db.query(query, [id], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Error deleting data from database");
+    } else {
+      res.status(200).send("Value Deleted");
+    }
+  });
+});
+// サーバーの起動
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Yey, your server is running on port ${port}`);
+});
